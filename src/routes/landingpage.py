7c2f3daefcd953919d7620ec85d753339d59965b
Juhanir <juhani.riisio@gmail.com>
2023-03-19 21:26:39 +0200

    add health endpoint

--- src/routes/landingpage.py
+++ src/routes/landingpage.py
@@ -5,3 +5,7 @@ from src.app import app
 @app.route("/", methods=["GET"])
 def landingpage():
     return render_template("index.html")
+
+@app.route("/health", methods=["GET"])
+def health():
+    return "ok"
