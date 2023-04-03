5160a97eb568d52d95bf2e819c2e74afd4003525
Juhanir <juhani.riisio@gmail.com>
2023-04-03 16:33:49 +0300

    improved flow in question creation

--- src/static/js/answer.js
+++ src/static/js/answer.js
@@ -1,22 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   const getAnswerCount = () => {
-    const answers = document.querySelectorAll("#answeropts input[type=text]");
-    return answers.length;
+    const answers = document.querySelectorAll("span[id^='answer_option']");
+    return answers ? answers.length : 0;
   };
-
-  const addAnswerBtn = document.querySelector("#add_answeropt");
   const answerOptsContainer = document.querySelector("#answeropts");
+  const addQuestionForm = document.querySelector("#add_question_form");
+  const addQuestionBtn = document.querySelector("#add_question");
+  const doneBtn = document.querySelector("#finish_creation");
+  const cancelQuestionBtn = document.querySelector("#cancel_question");
+  const addAnswerBtn = document.querySelector("#add_answeropt");
+  const removeAnswerBtn = document.querySelector("#remove_answeropt");
+
+  addQuestionBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    addQuestionBtn.style["display"] = "none";
+    doneBtn.style["display"] = "none";
+    addQuestionForm.style["display"] = "block";
+  });
+
   addAnswerBtn.addEventListener("click", (event) => {
     event.preventDefault();
     const index = getAnswerCount() + 1;
-    if (index >= 6) return;
+    if (index > 5) return;
     const span = document.createElement("span");
     span.setAttribute("id", `answer_option${index}`);
     span.innerHTML = `
-      <label class="block mb-1 md:mb-0 pr-4" for="answeropt${index}">
+      <label class="block md:mb-0 pr-4" for="answeropt${index}">
         Answer option ${index}
       </label>
-      <div class="md:flex md:items-center mb-6">
+      <div class="md:flex md:items-center mt-1">
         <div class="md:w-11/12">
           <input
             class="text-input"
@@ -38,5 +50,27 @@ document.addEventListener("DOMContentLoaded", () => {
       </div>
     `;
     answerOptsContainer.appendChild(span);
+  });
+
+  removeAnswerBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const answers = document.querySelectorAll("span[id^='answer_option']");
+    if (answers && answers.length > 2) {
+      answers[answers.length - 1].remove();
+    }
+  });
+
+  cancelQuestionBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    addQuestionForm.reset();
+    const answers = document.querySelectorAll("span[id^='answer_option']");
+    if (answers && answers.length > 2) {
+      for (let i = 2; i < answers.length; i++) {
+        answers[i].remove();
+      }
+    }
+    addQuestionForm.style["display"] = "none";
+    addQuestionBtn.style["display"] = "inline-block";
+    doneBtn.style["display"] = "inline-block";
   });
 });
